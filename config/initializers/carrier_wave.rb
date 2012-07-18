c86289adf934ae70ac455217fe8a09c1276f4598
shin.php <shin.php@gmail.com>
2012-07-18 17:24:36 +0800

    modified s3 account

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],
     :aws_secret_access_key  => ENV['S3_SECRET']
   }
-  config.fog_directory  = 'jf.pushsurvey.com'
-  config.fog_host       = 'http://jf.pushsurvey.com.s3.amazonaws.com'
+  config.fog_directory  = 'jiefu.pushsurvey.com'
+  config.fog_host       = 'http://jiefu.pushsurvey.com.s3.amazonaws.com'
   config.fog_attributes = { 'Cache-Control'=>'max-age=315576000' }
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
