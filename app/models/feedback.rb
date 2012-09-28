75e9babf2b6a8b5e0c9fade88508495353c97671
shin <shin.php@gmail.com>
2012-09-28 17:47:59 +0800

    'add_feedback_validates'

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Feedback < ActiveRecord::Base
   #validates_presence_of :product_name
+  validates :name,:comany,:department,:phone,:fax, :presence => true
+  validates :phone,:fax, :numericality => { :only_integer => true }
 end
